f3acd15726dbb3f339a39c5649fba4071d31a774
Taras Karpin <tarass777@hotmail.com>
2017-01-10 12:10:46 +0100

    Knapper Switches leds IO working test... ADC test not known

---
+++ ZYBO-master/tests/proj/hdmi_in.sdk/Vga_Main_V1/Debug/src/HDMI_IN/src/v2/display.d
@@ -0,0 +1,22 @@
+src/HDMI_IN/src/v2/display.o src/HDMI_IN/src/v2/display.o: \
+ ../src/HDMI_IN/src/v2/display.c ../src/HDMI_IN/src/v2/color.h \
+ ../src/HDMI_IN/src/v2/color.c ../src/HDMI_IN/src/v2/draw.h \
+ ../src/HDMI_IN/src/v2/draw.c ../src/HDMI_IN/src/v2/meta.h \
+ ../src/HDMI_IN/src/v2/display.h ../src/HDMI_IN/src/v2/display.c \
+ ../src/HDMI_IN/src/v2/meta.c
+
+../src/HDMI_IN/src/v2/color.h:
+
+../src/HDMI_IN/src/v2/color.c:
+
+../src/HDMI_IN/src/v2/draw.h:
+
+../src/HDMI_IN/src/v2/draw.c:
+
+../src/HDMI_IN/src/v2/meta.h:
+
+../src/HDMI_IN/src/v2/display.h:
+
+../src/HDMI_IN/src/v2/display.c:
+
+../src/HDMI_IN/src/v2/meta.c:
---
+++ ZYBO-master/tests/proj/hdmi_in.sdk/Vga_Main_V1/Debug/src/HDMI_IN/src/video_capture/video_capture.d
@@ -0,0 +1,81 @@
+src/HDMI_IN/src/video_capture/video_capture.o \
+ src/HDMI_IN/src/video_capture/video_capture.o: \
+ ../src/HDMI_IN/src/video_capture/video_capture.c \
+ ../src/HDMI_IN/src/video_capture/video_capture.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma_hw.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma_i.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xvtc.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xvtc_hw.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../src/HDMI_IN/src/video_capture/../intc/intc.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xdebug.h
+
+../src/HDMI_IN/src/video_capture/video_capture.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma_hw.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xaxivdma_i.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xvtc.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xvtc_hw.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../src/HDMI_IN/src/video_capture/../intc/intc.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xdebug.h:
