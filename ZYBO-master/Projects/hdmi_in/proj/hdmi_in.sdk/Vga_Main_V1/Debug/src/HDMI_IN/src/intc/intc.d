b1f5c8ce5843be795e7d9cc8f2ddffed4203116c
Taras Karpin <tarass777@hotmail.com>
2017-01-10 12:25:00 +0100

    git ignore slet

---
+++ ZYBO-master/Projects/hdmi_in/proj/hdmi_in.sdk/Vga_Main_V1/Debug/src/HDMI_IN/src/intc/intc.d
@@ -0,0 +1,49 @@
+src/HDMI_IN/src/intc/intc.o src/HDMI_IN/src/intc/intc.o: \
+ ../src/HDMI_IN/src/intc/intc.c ../src/HDMI_IN/src/intc/intc.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../src/HDMI_IN/src/intc/intc.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../Vga_Main_V1_bsp/ps7_cortexa9_0/include/xparameters.h:
