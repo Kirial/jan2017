724c3f57558d8f897a45e7e20eb816dcdb51c1b8
Taras Karpin <tarass777@hotmail.com>
2017-01-16 14:53:13 +0100

    test commit

---
+++ ZYBO-master/tests/proj/hdmi_in.sdk/TarasTest_bsp_xqspips_flash_lqspi_example_1/Debug/src/xqspips_flash_lqspi_example.d
@@ -0,0 +1,44 @@
+src/xqspips_flash_lqspi_example.o src/xqspips_flash_lqspi_example.o: \
+ ../src/xqspips_flash_lqspi_example.c \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xqspips.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xqspips_hw.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../TarasTest_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xqspips.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xqspips_hw.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../TarasTest_bsp/ps7_cortexa9_0/include/xil_printf.h:
