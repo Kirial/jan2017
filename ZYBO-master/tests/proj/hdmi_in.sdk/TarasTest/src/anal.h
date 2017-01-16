7a4bb74c9860e8438d1d0ac37ebb68c00d88b2ca
Taras Karpin <tarass777@hotmail.com>
2017-01-16 14:04:10 +0100

    test

---
+++ ZYBO-master/tests/proj/hdmi_in.sdk/TarasTest/src/anal.h
@@ -0,0 +1,14 @@
+/*
+ * anal.h
+ *
+ *  Created on: 16/01/2017
+ *      Author: taras
+ */
+
+#ifndef SRC_ANAL_H_
+#define SRC_ANAL_H_
+#include "platform.h"
+
+void set();
+
+#endif /* SRC_ANAL_H_ */
